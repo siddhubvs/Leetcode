216222b8e22477117dbf8d27fb49ba128c754e6c
siddhubvs <54083316+siddhubvs@users.noreply.github.com>
2021-05-08 20:02:30 +0530

    Create Simplified_ Fractions.java

---
+++ Simplified_ Fractions.java
@@ -0,0 +1,20 @@
+class Solution {
+    public List<String> simplifiedFractions(int n) {
+         List<String> ans = new ArrayList<>();
+        for(int i = 1;i<n;i++) {
+            for(int j=i+1;j<=n;j++) {
+                if(gcd(i,j) == 1) {
+                    ans.add(i + "/" + j);
+                }
+            }
+        }
+        return ans;
+    }
+    
+    int gcd(int a,int b) {
+        if(b == 0) {
+            return a;
+        }
+        return gcd(b,a%b);
+    }
+}
