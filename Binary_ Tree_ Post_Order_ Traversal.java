80120a20cb5681e992f0f7539e391b6b349c3bf9
siddhubvs <54083316+siddhubvs@users.noreply.github.com>
2021-05-08 22:05:26 +0530

    Create Binary_ Tree_ Post_Order_ Traversal.java

---
+++ Binary_ Tree_ Post_Order_ Traversal.java
@@ -0,0 +1,32 @@
+class Solution {
+    public List<Integer> postorderTraversal(TreeNode root) {
+        List<Integer> values = new ArrayList<>();
+        if (root == null) {
+            return values;
+        }
+
+        Stack<TreeNode> stack1 = new Stack<>();
+        Stack<TreeNode> stack2 = new Stack<>();
+
+        stack1.push(root);
+
+        while (!stack1.empty()) {
+            TreeNode temp = stack1.pop();
+            stack2.push(temp);
+
+            if (temp.left != null) {
+                stack1.push(temp.left);
+            }
+
+            if (temp.right != null) {
+                stack1.push(temp.right);
+            }
+        }
+
+        while (!stack2.empty()) {
+            values.add(stack2.pop().val);
+        }
+        
+        return values;
+    }
+}
