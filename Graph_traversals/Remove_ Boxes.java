65da70a2638031fcf9065ebdd3a6af7cf19b3ba6
siddhubvs <54083316+siddhubvs@users.noreply.github.com>
2021-06-06 22:27:39 +0530

    Create Remove_ Boxes.java
    You are given several boxes with different colors represented by different positive numbers.
    
    You may experience several rounds to remove boxes until there is no box left. Each time you can choose some continuous boxes with the same color (i.e., composed of k boxes, k >= 1), remove them and get k * k points.
    
    Return the maximum points you can get.
    
     
    
    Example 1:
    
    Input: boxes = [1,3,2,2,2,3,4,3,1]
    Output: 23
    Explanation:
    [1, 3, 2, 2, 2, 3, 4, 3, 1] 
    ----> [1, 3, 3, 4, 3, 1] (3*3=9 points) 
    ----> [1, 3, 3, 3, 1] (1*1=1 points) 
    ----> [1, 1] (3*3=9 points) 
    ----> [] (2*2=4 points)
    Example 2:
    
    Input: boxes = [1,1,1]
    Output: 9
    Example 3:
    
    Input: boxes = [1]
    Output: 1

---
+++ Graph_traversals/Remove_ Boxes.java
@@ -0,0 +1,32 @@
+class Solution {
+    public int removeBoxes(int[] boxes) {
+        int [][][] dp = new int [boxes.length][boxes.length][boxes.length];
+        return dfs(boxes, 0, boxes.length -1, 0, dp);
+    }
+    
+    private int dfs(int [] boxes, int start, int end, int prefix, int [][][] dp){
+        int res = 0;
+        int count = 0;
+        if(start > end)
+            return 0;
+        if(start == end)
+            return (prefix+1)*(prefix+1);
+        if(dp[start][end][prefix] != 0)
+            return dp[start][end][prefix];
+        int index = start;
+        while(index <= end && boxes[index] == boxes[start]){
+            index++;
+            count++;
+        }
+        int newStart = index;
+        res = (count+prefix)* (count+ prefix) + dfs(boxes, newStart, end, 0, dp);
+        while(index <= end){
+            if(boxes[index] == boxes[start] && boxes[index-1] != boxes[start]){
+                res = Math.max(res, dfs(boxes, newStart, index-1, 0, dp) + dfs(boxes, index, end, count+prefix, dp));
+            }
+            index++;
+        }
+        dp[start][end][prefix] = res;
+        return res; 
+    }
+}
